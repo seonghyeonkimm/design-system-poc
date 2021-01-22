25dd927102ad181cf96361bf76d6523927f7d15f
demian-at-066956696796 <demian@kcd.co.kr>
2021-01-22 12:51:30 +0900

    tailwindcss-design-system - Update storybook webpack.config.js

--- tailwindcss-design-system/.storybook/main.js
+++ tailwindcss-design-system/.storybook/main.js
@@ -1,7 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   stories: ['../components/**/*.stories.@(ts|tsx)'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    const isProduction = configType === 'PRODUCTION';
+
+    // rules
+    const cssRules = config.module.rules.find((rule) => rule.test.test('index.css'));
+    cssRules.include = path.resolve(__dirname, '../styles/index.css');
+    cssRules.use = [
+      isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+      { loader: "css-loader", options: { importLoaders: 1 } },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            config: path.resolve(__dirname, '../styles/postcss.config.js'),
+          },
+        },
+      },
+    ];
+
+    // plugins
+    if (isProduction) {
+      config.plugins.push(new MiniCssExtractPlugin())
+    }
+
+    return config;
+  },
 }
